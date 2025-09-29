b9e37f036a4d37fb984b687c299ddc1054055a98
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-09-29 11:40:23 -0300

    feat: cria o data base testes

---
+++ testes/acentos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE testes1 (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testes1 (nome)
+VALUES
+('João'),
+('Stéfanny')
+('Ângelo'),
+('Marcia'),
+('Júlia'),
+('Letícia'),
+('Vinícius');
